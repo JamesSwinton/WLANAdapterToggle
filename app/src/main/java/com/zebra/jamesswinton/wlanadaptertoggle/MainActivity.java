943cb760d7b5f4c756fdf4fd1a051955023d4fac
JS6845 <JS6845@zebra.com>
2019-11-19 14:04:11 +0000

    Added Pie Support

--- app/src/main/java/com/zebra/jamesswinton/wlanadaptertoggle/MainActivity.java
+++ app/src/main/java/com/zebra/jamesswinton/wlanadaptertoggle/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
   // Non-Static Variables
 
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
